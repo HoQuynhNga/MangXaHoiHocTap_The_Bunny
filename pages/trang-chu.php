76f7c267b6f8b35e87056fa574d0c4496c205857
datbt76 <buitandat1312@gmail.com>
2026-05-29 00:33:30 +0700

    Add Bootstrap and custom JavaScript to trang-chu.php

--- pages/trang-chu.php
+++ pages/trang-chu.php
@@ -516,6 +516,8 @@ $shortcuts = [
             <i class="fa-solid fa-user"></i><span>Hồ sơ</span>
         </button>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src='./assets/js/trang-chu.js'></script>
 </body>
 </html>
